fafecbbaf189f911ec3524372ba917fa053b15d5
Pradip219 <pradip@ht.com>
2022-03-03 19:16:53 +0530

    Test

--- Final.py
+++ Final.py
@@ -4,9 +4,9 @@ import json
 import os
 import requests
 print("Process Started")
-#subprocess.call("./Final.sh")
+subprocess.call("./Final.sh")
 data = {
     "text" : "Script has been executed completely"
 }
-webhook="https://hooks.slack.com/services/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"
+webhook="https://hooks.slack.com/services/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"
 requests.post(webhook,json.dumps(data))
